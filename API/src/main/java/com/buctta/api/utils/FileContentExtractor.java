6f391c5dc2411af6aaba47c10e4e315567b54c43
qq865578586 <865578586@qq.com>
2025-11-27 21:24:36 +0800

    Implement methods for parsing DOCX and PDF from InputStream
    Added methods to parse DOCX and PDF files from InputStream.

--- API/src/main/java/com/buctta/api/utils/FileContentExtractor.java
+++ API/src/main/java/com/buctta/api/utils/FileContentExtractor.java
@@ -7,6 +7,7 @@ import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FileContentExtractor {
 
@@ -35,5 +36,21 @@ public class FileContentExtractor {
         try (PDDocument doc = PDDocument.load(file)) {
             return new PDFTextStripper().getText(doc);
         }
+    }
+
+    /* ---------------- 工具方法 ---------------- */
+    public static String parseDocxByIS(InputStream in) throws Exception {
+        // 需要 poi-ooxml 依赖
+        try (var doc = new org.apache.poi.xwpf.usermodel.XWPFDocument(in)) {
+            var extractor = new org.apache.poi.xwpf.extractor.XWPFWordExtractor(doc);
+            return extractor.getText();
+        }
+    }
+
+    public static String parsePdfByIS(InputStream in) throws Exception {
+        // 需要 pdfbox 依赖
+        var document = org.apache.pdfbox.pdmodel.PDDocument.load(in);
+        var stripper = new org.apache.pdfbox.text.PDFTextStripper();
+        return stripper.getText(document);
     }
 }
